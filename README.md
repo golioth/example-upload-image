e025fbe6aaab47c16fd7e754a1dfb4551decd85a
Mike Szczys <mike@golioth.io>
2024-07-02 13:37:54 -0500

    initial commit
    Signed-off-by: Mike Szczys <mike@golioth.io>

---
+++ README.md
@@ -0,0 +1,113 @@
+# Golioth Example Upload Image
+
+## Overview
+
+This firmware uploads large files to an Amazon S3 bucket.
+
+The application will connect to Golioth and start sending Hello log
+messages every 5 seconds. After the fourth message a sample text file of
+2246 bytes will be uploaded as a way for you to verify the data route is
+configured correctly.
+
+## Supported Boards
+
+- Nordic nrf9160dk
+
+## Data Route Setup
+
+- Create an Amazon S3 bucket and generate a credential that allows
+  upload to it.
+- Use add the contents of the YAML file in the pipelines directory of
+  this repository to your Golioth project.
+- Use your S3 credential to set up the follow secrets in your Golioth
+  project:
+  - AWS_S3_ACCESS_KEY
+  - AWS_S3_ACCESS_SECRET
+
+You may follow the [Local Setup](#local-setup) guide to build your own
+firmware, or download and use the precompiled binary from the latest
+release. Note that you will need to [add Golioth PSK-ID/PSK to your
+device](#configure-authentication-credential) before it can connect to
+Golioth.
+
+## Local Setup
+
+> :important: Do not clone this repo using git. Zephyr's ``west`` meta
+> tool should be used to set up your local workspace.
+
+### Install the Python virtual environment
+
+```
+cd ~
+mkdir example-upload-image
+python -m venv example-upload-image/.venv
+source example-upload-image/.venv/bin/activate
+pip install wheel west
+```
+
+### Initialize and install
+
+Run one of these two initializations based on your target board:
+
+```
+# Initalize for Zephyr
+cd ~/example-upload-image
+west init -m git@github.com:golioth/example-template.git --mf west-zephyr.yml .
+
+# Initalize for NCS (Nordic boards only)
+cd ~/example-upload-image
+west init -m git@github.com:golioth/example-template.git --mf west-ncs.yml .
+
+```
+
+Fetch dependencies and configure the build environment
+
+```
+west update
+west zephyr-export
+pip install -r deps/zephyr/scripts/requirements.txt
+```
+
+## Building the application
+
+Prior to building, update ``CONFIG_MCUBOOT_IMGTOOL_SIGN_VERSION`` in the
+``prj.conf`` file to reflect the firmware version number you want to
+assign to this build.
+
+Then run the following commands to build and program the firmware based
+on the device you are using.
+
+### Zephyr build commands
+
+```
+$ (.venv) west build -p -b <my_board_name> --sysbuild app
+$ (.venv) west flash
+```
+
+### NCS build commands
+
+```
+$ (.venv) west build -p -b nrf9160dk_nrf9160_ns app
+$ (.venv) west flash
+```
+
+### Configure Authentication credential
+
+Configure PSK-ID and PSK using the device shell based on your Golioth
+credentials and reboot:
+
+```
+   uart:~$ settings set golioth/psk-id <my-psk-id@my-project>
+   uart:~$ settings set golioth/psk <my-psk>
+   uart:~$ kernel reboot cold
+```
+
+## Features
+
+This example implements the following Golioth services
+
+* Runtime credentials
+* Backend Logging
+* Device Settings
+* OTA Firmware Update
+* Stream data using block upload
---
+++ src/main.c
@@ -0,0 +1,202 @@
+/*
+ * Copyright (c) 2022-2023 Golioth, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "golioth/golioth_status.h"
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(example_upload_image, LOG_LEVEL_DBG);
+
+#include <golioth/client.h>
+#include <golioth/fw_update.h>
+#include <golioth/settings.h>
+#include <golioth/stream.h>
+#include <samples/common/net_connect.h>
+#include <samples/common/sample_credentials.h>
+#include <zephyr/kernel.h>
+
+#include "fables.h"
+
+/* Current firmware version; update in prj.conf or via build argument */
+static const char *_current_version = CONFIG_MCUBOOT_IMGTOOL_SIGN_VERSION;
+
+static struct golioth_client *client;
+K_SEM_DEFINE(connected, 0, 1);
+
+static k_tid_t _system_thread = 0;
+
+static int32_t _loop_delay_s = 5;
+#define LOOP_DELAY_S_MAX 43200
+#define LOOP_DELAY_S_MIN 0
+
+#define BU_SIZE 1024
+
+struct block_upload_source
+{
+    uint8_t *buf;
+    size_t len;
+};
+
+static void wake_system_thread(void)
+{
+    k_wakeup(_system_thread);
+}
+
+static void on_client_event(struct golioth_client *client,
+                            enum golioth_client_event event,
+                            void *arg)
+{
+    bool is_connected = (event == GOLIOTH_CLIENT_EVENT_CONNECTED);
+
+    if (is_connected)
+    {
+        k_sem_give(&connected);
+    }
+    LOG_INF("Golioth client %s", is_connected ? "connected" : "disconnected");
+}
+
+static enum golioth_settings_status on_loop_delay_setting(int32_t new_value, void *arg)
+{
+    _loop_delay_s = new_value;
+    LOG_INF("Set loop delay to %i seconds", new_value);
+    wake_system_thread();
+    return GOLIOTH_SETTINGS_SUCCESS;
+}
+
+static int app_settings_register(struct golioth_client *client)
+{
+    struct golioth_settings *settings = golioth_settings_init(client);
+
+    int err = golioth_settings_register_int_with_range(settings,
+                                                       "LOOP_DELAY_S",
+                                                       LOOP_DELAY_S_MIN,
+                                                       LOOP_DELAY_S_MAX,
+                                                       on_loop_delay_setting,
+                                                       NULL);
+
+    if (err)
+    {
+        LOG_ERR("Failed to register settings callback: %d", err);
+    }
+
+    return err;
+}
+
+/* Callback invoked by the Golioth SDK to read the next block of data
+   data to send to Golioth */
+static enum golioth_status block_upload_read_chunk(uint32_t block_idx,
+                                                   uint8_t *block_buffer,
+                                                   size_t *block_size,
+                                                   bool *is_last,
+                                                   void *arg)
+{
+    const struct block_upload_source *bu_source = arg;
+    size_t bu_offset = block_idx * BU_SIZE;
+    size_t bu_size = bu_source->len - bu_offset;
+
+    LOG_DBG("block-idx: %u bu_offset: %u bytes_remaining: %u", block_idx, bu_offset, bu_size);
+
+    if (bu_offset >= bu_source->len)
+    {
+        LOG_ERR("Calculated offset is past end of buffer: %d", bu_offset);
+        goto bu_error;
+    }
+
+    if (bu_size < 0)
+    {
+        LOG_ERR("Calculated size for next block is < 0: %d", bu_size);
+        goto bu_error;
+    }
+
+    if (bu_size <= BU_SIZE)
+    {
+        *block_size = bu_size;
+        *is_last = true;
+    }
+    else
+    {
+        *block_size = BU_SIZE;
+        *is_last = false;
+    }
+
+    memcpy(block_buffer, bu_source->buf + bu_offset, *block_size);
+    return GOLIOTH_OK;
+
+bu_error:
+    *block_size = 0;
+    *is_last = true;
+
+    return GOLIOTH_ERR_NO_MORE_DATA;
+}
+
+static int upload_txt_file(void)
+{
+    /* Test function that uses block upload with a .txt file */
+    struct block_upload_source bu_ctx = {.buf = (uint8_t *) &fables, .len = fables_len};
+
+    int err = golioth_stream_set_blockwise_sync(client,
+                                                "blockupload",
+                                                GOLIOTH_CONTENT_TYPE_OCTET_STREAM,
+                                                block_upload_read_chunk,
+                                                &bu_ctx);
+
+    return err;
+}
+
+int main(void)
+{
+    LOG_DBG("Start Golioth example_upload_image");
+    LOG_INF("Firmware version: %s", CONFIG_MCUBOOT_IMGTOOL_SIGN_VERSION);
+
+    /* Get system thread id so loop delay change event can wake main */
+    _system_thread = k_current_get();
+
+    /* Start the network connection */
+    net_connect();
+
+    /* Get the client configuration from auto-loaded settings */
+    const struct golioth_client_config *client_config = golioth_sample_credentials_get();
+
+    /* Create and start a Golioth Client */
+    client = golioth_client_create(client_config);
+
+    /* Register Golioth event callback */
+    golioth_client_register_event_callback(client, on_client_event, NULL);
+
+    /* Initialize Golioth OTA firmware update service */
+    golioth_fw_update_init(client, _current_version);
+
+    /* Register Golioth Settings service */
+    app_settings_register(client);
+
+    /* Block until connected to Golioth */
+    k_sem_take(&connected, K_FOREVER);
+
+    int counter = 0;
+    int err;
+
+    while (true)
+    {
+        LOG_INF("Golioth hello! %d", counter);
+
+        char buf[32];
+        snprintk(buf, sizeof(buf), "{\"counter\":%d}", counter);
+
+        if (counter == 4)
+        {
+            err = upload_txt_file();
+            if (err)
+            {
+                LOG_ERR("Error during block upload: %d", err);
+            }
+            else
+            {
+                LOG_INF("Block upload successful!");
+            }
+        }
+
+        ++counter;
+        k_sleep(K_SECONDS(_loop_delay_s));
+    }
+}
